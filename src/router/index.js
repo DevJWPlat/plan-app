9586891a3f195c04d6b415157c8d49164c5f3f98
DevJWPlat <166898654+DevJWPlat@users.noreply.github.com>
2026-06-21 12:36:14 +0100

    Initial Vue setup

---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import HomeView from '../views/HomeView.vue'
+import MapView from '../views/MapView.vue'
+import RouteView from '../views/RouteView.vue'
+import VotesView from '../views/VotesView.vue'
+import PricingView from '../views/PricingView.vue'
+import LoginView from '../views/LoginView.vue'
+import AdminView from '../views/AdminView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: HomeView
+  },
+  {
+    path: '/login',
+    component: LoginView
+  },
+  {
+    path: '/map',
+    component: MapView
+  },
+  {
+    path: '/route',
+    component: RouteView
+  },
+  {
+    path: '/votes',
+    component: VotesView
+  },
+  {
+    path: '/pricing',
+    component: PricingView
+  },
+  {
+    path: '/admin',
+    component: AdminView
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: NotFoundView
+  }
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes
+})
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="p-6">
+      <h1 class="text-3xl font-bold">
+        Balkans Road Trip Planner
+      </h1>
+  
+      <p class="mt-4">
+        Welcome to Plan App
+      </p>
+    </div>
+</template>
